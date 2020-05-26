6d2d9b2ebea592f914fc0262b30d8e601bf17753
Angel <angelsalvadordev@gmail.com>
2020-05-26 16:55:27 -0500

    replace docs

---
+++ docs/precache-manifest.77b3ac0f0e87eda4139fa195b89a46c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f50fe3a19a34869d3935f49098d23f50",
+    "url": "/knight-ajedrez/index.html"
+  },
+  {
+    "revision": "e8ac2746d2683ad0c6e8",
+    "url": "/knight-ajedrez/static/js/2.67136b13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/knight-ajedrez/static/js/2.67136b13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3b3b7cc6c3c38b8bf64",
+    "url": "/knight-ajedrez/static/js/main.fb8f6d84.chunk.js"
+  },
+  {
+    "revision": "4f2db3fdc6094d705bdd",
+    "url": "/knight-ajedrez/static/js/runtime-main.6f2e64d9.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d12841fc9b0e0aeeb45ad677b4637ad8.js"
+  "/knight-ajedrez/precache-manifest.77b3ac0f0e87eda4139fa195b89a46c2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/knight-ajedrez/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
